954f20f34ae3be7dd009d773c5679cca57f85a3a
art <partypeopleland@gmail.com>
2016-05-22 11:20:00 +0800

    採用event改寫

--- CSharpEventDemo/Program.cs
+++ CSharpEventDemo/Program.cs
@@ -10,38 +10,44 @@ namespace CSharpEventDemo
     {
         static void Main(string[] args)
         {
-            // 使用Delegate完成Observer pattern
-            Console.WriteLine("Delegate Demo");
-            var tempatureMonitorDelegate = new TempatureMonitorUsingDelegate();
+            // 使用Event事件委派
+            Console.WriteLine("Event Demo");
+            var tempatureMonitorEvent = new TempatureMonitorUsingEvent();
+
             var desktopApp = new DesktopApp();
             var mobileApp = new MobileApp();
-            tempatureMonitorDelegate.OnTempatureChanged += desktopApp.OnTempatureChanged;
-            tempatureMonitorDelegate.OnTempatureChanged += mobileApp.OnTempatureChanged;
+            // 額外自訂事件委派方法, 由於是宣告成事件委派, 輸入到+=時可以直接用TAB產生基本的程式碼
+            tempatureMonitorEvent.OnTempatureChanged += TempatureMonitorEvent_OnTempatureChanged;
+            tempatureMonitorEvent.OnTempatureChanged += desktopApp.OnTempatureChangedEvent;
+            tempatureMonitorEvent.OnTempatureChanged += mobileApp.OnTempatureChangedEvent;
 
-            Console.WriteLine("設定溫度，現在是30.5度");
-            tempatureMonitorDelegate.Tempature = 30.5;
+            Console.WriteLine("溫度變化了，現在是30.5度");
+            tempatureMonitorEvent.Tempature = 30.5;
 
-            Console.WriteLine("設定溫度，現在依然是30.5度");
-            tempatureMonitorDelegate.Tempature = 30.5;
+            Console.WriteLine("溫度沒變化，現在依然是30.5度");
+            tempatureMonitorEvent.Tempature = 30.5;
 
-            Console.WriteLine("設定溫度，現在是28.6度");
-            tempatureMonitorDelegate.Tempature = 28.6;
+            Console.WriteLine("溫度變化了，現在是28.6度");
+            tempatureMonitorEvent.Tempature = 28.6;
 
             Console.WriteLine("mobileApp不再想觀察了");
-            tempatureMonitorDelegate.OnTempatureChanged -= mobileApp.OnTempatureChanged;
+            tempatureMonitorEvent.OnTempatureChanged -= mobileApp.OnTempatureChangedEvent;
 
-            Console.WriteLine("設定溫度，現在是27.6度");
-            tempatureMonitorDelegate.Tempature = 27.6;
+            Console.WriteLine("溫度變化了，現在是27.6度");
+            tempatureMonitorEvent.Tempature = 27.6;
             Console.WriteLine();
             Console.ReadKey();
         }
+        private static void TempatureMonitorEvent_OnTempatureChanged(object sender, double e)
+        {
+            Console.WriteLine($"---自訂的委派方法得知溫度變化了: {e}");
+        }
     }
 
-    public partial class TempatureMonitorUsingDelegate
+    public class TempatureMonitorUsingEvent
     {
-        public delegate void TempatureChangedHandler(double tempature);
-
-        public TempatureChangedHandler OnTempatureChanged;
+        // 使用EventHandler<T>來省去自訂delegate的麻煩
+        public event EventHandler<double> OnTempatureChanged;
 
         private double tempature;
 
@@ -54,22 +60,22 @@ namespace CSharpEventDemo
                 if (oldTempature != value)
                 {
                     tempature = value;
-                    OnTempatureChanged.Invoke(value);
+                    if (OnTempatureChanged != null)
+                    {
+                        OnTempatureChanged(this, value);
+                    }
                 }
             }
         }
-
-        public TempatureMonitorUsingDelegate()
-        {
-            // 使用delegate必須給定一個初始的委派方法
-            OnTempatureChanged = tempatureChanged;
-        }
-
-        private void tempatureChanged(double tempature)
-        {
-            Console.WriteLine($"---委派方法內偵測到溫度發生變化了...{tempature}");
-        }
     }
+
+    
+    public interface ITempatureMonitorEvent
+    {
+        void OnTempatureChangedEvent(object sender, double e);
+    }
+
+
     // ITempatureMonitorSubject.cs
     public interface ITempatureMonitorSubject
     {
@@ -85,20 +91,32 @@ namespace CSharpEventDemo
     {
         void OnTempatureChanged(double tempature);
     }
-    public class DesktopApp : ITempatureMonitorObserver
+    // DesktopApp.cs
+    public class DesktopApp : ITempatureMonitorObserver, ITempatureMonitorEvent
     {
         public void OnTempatureChanged(double tempature)
         {
-            Console.WriteLine($"------Desktop App被通知溫度變化了: {tempature}");
+            Console.WriteLine($"---Desktop App被通知溫度變化了: {tempature}");
+        }
+
+        public void OnTempatureChangedEvent(object sender, double tempature)
+        {
+            Console.WriteLine($"---Desktop App使用事件委派方法得知溫度變化了: {tempature}");
         }
     }
 
+
     // MobileApp.cs
-    public class MobileApp : ITempatureMonitorObserver
+    public class MobileApp : ITempatureMonitorObserver, ITempatureMonitorEvent
     {
         public void OnTempatureChanged(double tempature)
         {
-            Console.WriteLine($"------Mobile App被通知溫度變化了: {tempature}");
+            Console.WriteLine($"---Mobile App被通知溫度變化了: {tempature}");
+        }
+
+        public void OnTempatureChangedEvent(object sender, double tempature)
+        {
+            Console.WriteLine($"---Mobile App使用事件委派方法得知溫度變化了: {tempature}");
         }
     }
 }
